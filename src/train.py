ac758f05daae15135b3c2d114f31885e15245363
José Benavente <jose@bgm.cl>
2024-12-15 10:47:29 -0300

    update project to python 3.7

--- src/train.py
+++ src/train.py
@@ -1,33 +1,45 @@
+from sklearn.metrics import roc_auc_score
+
 from dkn import DKN
-import tensorflow as tf
 import numpy as np
 
+import numpy as np
+import tensorflow as tf
 
-def get_feed_dict(model, data, start, end):
-    feed_dict = {model.clicked_words: data.clicked_words[start:end],
-                 model.clicked_entities: data.clicked_entities[start:end],
-                 model.news_words: data.news_words[start:end],
-                 model.news_entities: data.news_entities[start:end],
-                 model.labels: data.labels[start:end]}
-    return feed_dict
+
+def get_batch_data(data, start, end):
+    return {
+        'clicked_words': data.clicked_words[start:end],
+        'clicked_entities': data.clicked_entities[start:end],
+        'news_words': data.news_words[start:end],
+        'news_entities': data.news_entities[start:end],
+        'labels': data.labels[start:end]
+    }
 
 
 def train(args, train_data, test_data):
+    # Create and compile model
     model = DKN(args)
+    model.compile(args)
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(tf.local_variables_initializer())
+    for epoch in range(args.n_epochs):
+        # Training
+        start_list = list(range(0, train_data.size, args.batch_size))
+        np.random.shuffle(start_list)
+        total_loss = 0
 
-        for step in range(args.n_epochs):
-            # training
-            start_list = list(range(0, train_data.size, args.batch_size))
-            np.random.shuffle(start_list)
-            for start in start_list:
-                end = start + args.batch_size
-                model.train(sess, get_feed_dict(model, train_data, start, end))
+        for start in start_list:
+            end = min(start + args.batch_size, train_data.size)
+            batch_data = get_batch_data(train_data, start, end)
+            loss = model.train_step(batch_data)
+            total_loss += loss
 
-            # evaluation
-            train_auc = model.eval(sess, get_feed_dict(model, train_data, 0, train_data.size))
-            test_auc = model.eval(sess, get_feed_dict(model, test_data, 0, test_data.size))
-            print('epoch %d    train_auc: %.4f    test_auc: %.4f' % (step, train_auc, test_auc))
+        # Evaluation
+        train_pred = model.test_step(get_batch_data(train_data, 0, train_data.size))
+        test_pred = model.test_step(get_batch_data(test_data, 0, test_data.size))
+
+        train_auc = roc_auc_score(train_data.labels, train_pred)
+        test_auc = roc_auc_score(test_data.labels, test_pred)
+
+        print(f'epoch {epoch}    train_auc: {train_auc:.4f}    test_auc: {test_auc:.4f}    '
+              f'avg_loss: {total_loss / len(start_list):.4f}')
